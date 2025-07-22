ecb08259ead2323e4a32b95fa0ca406356aa3f06
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-07-22 12:54:32 -0400

    AbstractUser in progress.

--- ARHuntBackend/myapp/models.py
+++ ARHuntBackend/myapp/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+    score = models.IntegerField(default=0)
+    grade = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.email
 
 class User(models.Model):
     score = models.IntegerField(default=0)
-    email = models.EmailField(unique=True, help_text="Enter a valid email address.")
+    email = models.EmailField(unique=True)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     grade = models.IntegerField(default=0)
--- ARHuntBackend/mysite/settings.py
+++ ARHuntBackend/mysite/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
