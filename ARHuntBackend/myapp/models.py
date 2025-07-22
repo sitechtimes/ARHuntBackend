b2d7a08f2a616fb3dcb8df565ad56499f7d97d0e
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-07-22 12:26:00 -0400

    username

--- ARHuntBackend/myapp/models.py
+++ ARHuntBackend/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
     score = models.IntegerField(default=0)
     email = models.EmailField(unique=True, help_text="Enter a valid email address.")
+    username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     grade = models.IntegerField(default=0)
     def __str__(self):
