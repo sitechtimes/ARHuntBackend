f88454bfcf897215b69b9ae10335381b5904e57d
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-23 11:02:02 -0400

    3 create rats model (#5)
    Co-authored-by: Kenneth Ng <145921389+kennething@users.noreply.github.com>

---
+++ ARHuntBackend/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Rat(models.Model):
+    rat_type = models.CharField()
+    scale = models.CharField()
+    caught = models.BooleanField(default = False)
+    score = models.IntegerField()
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
---
+++ ARHuntBackend/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView,Response
+
+# Create your views here.
+class RatView(APIView):
+    def get(self, request):
+        rats = Rat.objects.all()
+        return Response(RatSerializer(rats, many=True).data)
+
+    def post(self, request):
+        serializer = RatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
