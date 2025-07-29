b49b3b4dd0205053bf52de28890b5de65b669730
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-29 12:58:37 -0400

    l

--- ARHuntBackend/core/signals.py
+++ ARHuntBackend/core/signals.py
@@ -1,17 +1,15 @@
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import post_save, pre_delete
 from .models import *
 from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
 
-@receiver(pre_save, sender=Rat)
-def update_score(sender, instance, **kwargs):
-    if instance.id is None:
-        return
+@receiver(post_save, sender=Rat)
+def update_score(sender, created, instance, **kwargs):
     previous = Rat.objects.get(rat_id=instance.rat_id)
 
-    if not previous.caught:
+    if not previous.caught and not created:
         user = get_object_or_404(CustomUser, pk=instance.user_id)
         user.score += instance.score
         user.save()
