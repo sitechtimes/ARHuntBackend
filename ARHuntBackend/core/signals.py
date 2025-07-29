a42248bb7a2f51c50762761ba5a07613046d7ca1
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-29 08:18:58 -0400

    e

--- ARHuntBackend/core/signals.py
+++ ARHuntBackend/core/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save, pre_delete
-from django.contrib.auth.models import Rat
-from django.dispatch import receiver
+# from django.db.models.signals import post_save, pre_delete
+# from django.contrib.auth.models import Rat
+# from django.dispatch import receiver
 
-@receiver(post_save, sender=Rat) 
-def update_score(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=Rat) 
+# def update_score(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
  
