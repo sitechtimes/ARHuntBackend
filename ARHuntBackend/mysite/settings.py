908b5a62068efa037eefcde3d94d06ee12aa7fed
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-08-04 12:33:36 -0400

    Update settings.py
    @Jarvis-Li2244 Told me to do this bc hes lazy and dont wanna open a PR if it breaks bc of the one line changed ITS HIS FAULT

--- ARHuntBackend/mysite/settings.py
+++ ARHuntBackend/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hzzi3vno_#ia1abep6x&w5=wf2blulojo__ai+dlt=@+)!+ak_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["100.101.65.62", "127.0.0.1"]
+ALLOWED_HOSTS = ["100.101.65.95", "127.0.0.1", "100.101.78.71"]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
